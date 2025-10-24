7f49ad1f664e4829c5b1ece02f526cebe510caf1
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2025-10-24 17:20:40 +0200

    Modifcar elements de una constante a una funcion

--- frontend/js/dom.js
+++ frontend/js/dom.js
@@ -1,17 +1,23 @@
-export const elements = {
-  appDescription: document.getElementById("appDescription"),
-  generateBtn: document.getElementById("generateBtn"),
-  clearBtn: document.getElementById("clearBtn"),
-  diagramContainer: document.getElementById("diagramContainer"),
-  descriptionContainer: document.getElementById("descriptionContainer"),
-  loading: document.getElementById("loading"),
-};
+// Asegurar que se ejecute después del DOMContentLoaded
+export function getElements() {
+  // TODO: hacer refactor, no puede ser que siempre me esté devolviendo este carro de elementos
+  return {
+    appDescription: document.getElementById("appDescription"),
+    generateBtn: document.getElementById("generateBtn"),
+    clearBtn: document.getElementById("clearBtn"),
+    resetBtn: document.getElementById("resetBtn"),
+    diagramContainer: document.getElementById("diagramContainer"),
+    descriptionContainer: document.getElementById("descriptionContainer"),
+    loading: document.getElementById("loading"),
+    outputDescription: document.getElementById("outputDescription"),
+  };
+}
 
 export function resetOutput() {
+  elements.appDescription.value = "";
   elements.diagramContainer.classList.add("empty");
   elements.diagramContainer.innerHTML =
     "<p>Your blueprint diagram will appear here</p>";
-
   elements.descriptionContainer.classList.add("empty");
   elements.descriptionContainer.innerHTML =
     "<p>Blueprint description will appear here</p>";
