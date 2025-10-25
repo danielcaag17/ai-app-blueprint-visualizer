9d6477f22e502a5501450c5bc7ba20bb9ac722c8
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2025-10-25 13:14:36 +0200

    Cambio en las rutas centralizadas en router.py

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,41 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routes.full_blueprint import router as full_blueprint
-from backend.routes.blueprint import router as generate_blueprint
-from backend.routes.generate_structure import router as generate_structure
+from backend.core.config import settings
+from backend.router import router as api_router
 
-app = FastAPI(title="AI App Blueprint Visualizer API")
+app = FastAPI(
+    title=settings.APP_NAME,
+    description="API que genera diagramas UML en formato Mermaid a partir de descripciones en lenguaje natural.",
+    version="0.1.0"
+)
 
 # Habilita CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:3000"],  # o ["*"] para permitir todo (no recomendado en producción)
+    allow_origins=settings.ALLOWED_ORIGINS,  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(full_blueprint, prefix="/blueprint/full", tags=["generate_full_blueprint"])
-app.include_router(generate_blueprint, prefix="/blueprint/analyze", tags=["generate_blueprint"])
-app.include_router(generate_structure, prefix="/bluerprint/generate", tags=["generate_structure"])
+# Incluir rutas de la API
+app.include_router(api_router, prefix="/api")
+
+
+# Evento al iniciar la aplicación (útil para precargar modelos)
+# TODO: cambiar on_event
+@app.on_event("startup")
+async def startup_event():
+    print("🚀 Inicializando API y cargando modelos...")
+    # Podrías cargar tus modelos globales aquí si lo deseas
+
+# Evento al apagar la aplicación (útil para limpiar recursos)
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("🛑 Cerrando API y liberando recursos...")
+
 
 @app.get("/")
 async def root():
     return {"message": "AI App Blueprint Visualizer API is running"}
+
---
+++ backend/routes/__init__.py
@@ -0,0 +1,3 @@
+from . import blueprint, full_blueprint, generate_structure
+
+__all__ = ["blueprint", "full_blueprint", "generate_structure"]
