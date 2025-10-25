da981a866f0071ea58f9c1c8b0396bff9f72c055
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2025-10-25 15:52:51 +0200

    cambio de los tags del router

--- backend/router.py
+++ backend/router.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 from backend.routes.full_blueprint import router as full_blueprint
 from backend.routes.blueprint import router as generate_blueprint
-from backend.routes.generate_structure import router as generate_structure
+from backend.routes.structure import router as generate_structure
 
 router = APIRouter()
 
 # Registrar subrouters
-router.include_router(full_blueprint, prefix="/blueprint/full", tags=["generate_full_blueprint"])
-router.include_router(generate_blueprint, prefix="/blueprint/analyze", tags=["generate_blueprint"])
-router.include_router(generate_structure, prefix="/bluerprint/generate", tags=["generate_structure"])
+router.include_router(full_blueprint, prefix="/full-blueprint", tags=["blueprint", "structure"])
+router.include_router(generate_blueprint, prefix="/blueprint", tags=["blueprint"])
+router.include_router(generate_structure, prefix="/structure", tags=["structure"])
 
