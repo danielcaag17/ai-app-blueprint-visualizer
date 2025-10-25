5dfe19889db90df4ad8acde558c1eae979377dd0
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2025-10-25 19:07:17 +0200

    Gestionador de ML models

--- backend/core/model_manager.py
+++ backend/core/model_manager.py
@@ -1,17 +1,17 @@
 # app/core/model_manager.py
 from typing import Optional
-from backend.ml_models.entity_extractor.model import EntityExtractor
-from backend.ml_models.relation_extractor.model import RelationExtractor
+from backend.ml_models.entity_extractor.model import EntityLLMExtractor
+from backend.ml_models.relation_extractor.model import RelationLLMExtractor
 
 class ModelManager:
     def __init__(self):
-        self.entity_model: Optional[EntityExtractor] = None
-        self.relation_model: Optional[RelationExtractor] = None
+        self.entity_model: Optional[EntityLLMExtractor] = None
+        self.relation_model: Optional[RelationLLMExtractor] = None
 
     async def load_models(self):
         print("🚀 Cargando modelos NLP...")
-        self.entity_model = EntityExtractor("backend/ml_models/entity_extractor/")
-        self.relation_model = RelationExtractor("backend/ml_models/relation_extractor/")
+        self.entity_model = EntityLLMExtractor("backend/ml_models/entity_extractor/")
+        self.relation_model = RelationLLMExtractor("backend/ml_models/relation_extractor/")
         print("✅ Modelos cargados correctamente")
 
     async def unload_models(self):
@@ -23,11 +23,3 @@ class ModelManager:
 
 # Instancia global que se puede importar desde cualquier módulo
 model_manager = ModelManager()
-
-'''
-Ejemplo de uso:
-from app.core.model_manager import model_manager
-
-def extract_entities(text: str):
-    return model_manager.entity_model.extract(text)
-'''
