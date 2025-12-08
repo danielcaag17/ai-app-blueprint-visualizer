8b56f18d22a08ac74b0c2ce26bb04cbe33cb15a5
Daniel Cañizares Aguilar <danielcanyizares@gmail.com>
2025-12-08 19:25:10 +0100

    Eliminado mensajes de log

--- backend/core/model_manager.py
+++ backend/core/model_manager.py
@@ -9,17 +9,13 @@ class ModelManager:
         self.relation_model: Optional[RelationLLMExtractor] = None
 
     async def load_models(self):
-        print("🚀 Cargando modelos NLP...")
         self.entity_model = EntityLLMExtractor("backend/ml_models/entity_extractor/prod/")
         # Not implemented yet
         # self.relation_model = RelationLLMExtractor("backend/ml_models/relation_extractor/prod/")
-        print("✅ Modelos cargados correctamente")
 
     async def unload_models(self):
-        print("🧹 Liberando modelos NLP...")
         self.entity_model = None
         self.relation_model = None
-        print("✅ Modelos liberados correctamente")
 
 
 # Instancia global que se puede importar desde cualquier módulo
